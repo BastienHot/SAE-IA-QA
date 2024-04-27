90bf2cc110e3bd47413afeda59cac99a9b1fd762
Fatih <fatih.fidan12@gmail.com>
2024-04-27 15:22:54 +0200

    maj main.py v1.3

--- application/Backend/main.py
+++ application/Backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from Controller.Controller_IA import prediction_blueprint
-from Controller.Controller_Database import signup_blueprint, login_blueprint, user_chat_history_blueprint, delete_chat_blueprint, chat_message_blueprint
+from Controllers.Controller_IA import prediction_blueprint
+from Controllers.Controller_Database import signup_blueprint, login_blueprint, user_chat_history_blueprint, delete_chat_blueprint, chat_message_blueprint
 from Model.IA import IA
 
 app = Flask(__name__)
